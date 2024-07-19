7fb8a87f17d3b07b3d5cb2189308f698aca8870d
openmpy <suhwan@kakao.com>
2024-07-19 17:20:36 +0900

    feat: Add initial SpringBoot project structure

---
+++ src/main/java/com/twoclock/gitconnect/GitconnectApplication.java
@@ -0,0 +1,13 @@
+package com.twoclock.gitconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitconnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/twoclock/gitconnect/GitconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twoclock.gitconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitconnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
