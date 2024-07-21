4c537cd454615c0a9b0edf2cc33739bd7732b85c
jjangsky <dbcks4646@gmail.com>
2024-07-21 20:54:19 +0900

    feat: add BaseEnitiy  to automatically check regist and modity times of entities

---
+++ src/main/java/com/twoclock/gitconnect/global/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.twoclock.gitconnect.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedDateTime;
+}
