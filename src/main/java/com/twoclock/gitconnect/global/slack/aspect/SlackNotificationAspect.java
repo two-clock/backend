3a8eb84baffe58be0131d7269a7114cbfb3a83dd
openmpy <suhwan@kakao.com>
2024-07-23 01:18:28 +0900

    feat: Add ThreadPoolConfig and update SlackNotificationAspect

--- src/main/java/com/twoclock/gitconnect/global/slack/aspect/SlackNotificationAspect.java
+++ src/main/java/com/twoclock/gitconnect/global/slack/aspect/SlackNotificationAspect.java
@@ -10,19 +10,22 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Date;
 
 @RequiredArgsConstructor
-@Profile(value = "local")
+@Profile(value = "dev")
 @Aspect
 @Component
 public class SlackNotificationAspect {
 
     private final SlackApi slackApi;
+    private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
     @Around(
             value = "@annotation(com.twoclock.gitconnect.global.slack.annotation.SlackNotification) && args(request, e)",
@@ -31,13 +34,14 @@ public class SlackNotificationAspect {
     public Object slackNotification(
             ProceedingJoinPoint proceedingJoinPoint, HttpServletRequest request, Exception e
     ) throws Throwable {
-        Object proceed = proceedingJoinPoint.proceed();
-        sendSlackErrorMessage(request, e);
+        threadPoolTaskExecutor.execute(() -> sendSlackErrorMessage(request, e));
 
-        return proceed;
+        return proceedingJoinPoint.proceed();
     }
 
     private void sendSlackErrorMessage(HttpServletRequest request, Exception e) {
+        String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+
         SlackAttachment slackAttachment = new SlackAttachment();
         slackAttachment.setFallback("Error");
         slackAttachment.setColor("danger");
@@ -49,7 +53,7 @@ public class SlackNotificationAspect {
                 Arrays.asList(
                         new SlackField().setTitle("Request URL").setValue(request.getRequestURL().toString()),
                         new SlackField().setTitle("Request Method").setValue(request.getMethod()),
-                        new SlackField().setTitle("Request Time").setValue(new Date().toString()),
+                        new SlackField().setTitle("Request Time").setValue(now),
                         new SlackField().setTitle("Request IP").setValue(request.getRemoteAddr()),
                         new SlackField().setTitle("Request User-Agent").setValue(request.getHeader("User-Agent"))
                 )
@@ -59,7 +63,7 @@ public class SlackNotificationAspect {
         slackMessage.setAttachments(Collections.singletonList(slackAttachment));
         slackMessage.setIcon(":x:");
         slackMessage.setText("에러가 발생했습니다.");
-        slackMessage.setUsername("에러봇");
+        slackMessage.setUsername("에러 봇");
         slackApi.call(slackMessage);
     }
 }
