8bf04730bf7bdd77122f01f3f9a4482c24d2853b
jjangsky <dbcks4646@gmail.com>
2024-07-29 23:23:53 +0900

    feat: modification to set Comment Entity relationship

--- src/main/java/com/twoclock/gitconnect/domain/comment/entity/Comment.java
+++ src/main/java/com/twoclock/gitconnect/domain/comment/entity/Comment.java
@@ -1,11 +1,11 @@
 package com.twoclock.gitconnect.domain.comment.entity;
 
+import com.twoclock.gitconnect.domain.board.entity.Board;
+import com.twoclock.gitconnect.domain.member.entity.Member;
 import com.twoclock.gitconnect.global.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,4 +17,30 @@ public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String isView;
+
+    @Builder
+    public Comment(Board board, Member member, String nickname, String content, String isView) {
+        this.board = board;
+        this.member = member;
+        this.nickname = nickname;
+        this.content = content;
+        this.isView = isView;
+    }
 }
