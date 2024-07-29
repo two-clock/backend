537da9182443005f12467a1bfe70685640c5f3f3
jjangsky <dbcks4646@gmail.com>
2024-07-29 23:09:44 +0900

    feat: modifty Board Entity relationship

--- src/main/java/com/twoclock/gitconnect/domain/board/entity/Board.java
+++ src/main/java/com/twoclock/gitconnect/domain/board/entity/Board.java
@@ -1,11 +1,12 @@
 package com.twoclock.gitconnect.domain.board.entity;
 
+import com.twoclock.gitconnect.domain.board.entity.constants.Category;
+import com.twoclock.gitconnect.domain.member.entity.Member;
+import com.twoclock.gitconnect.domain.member.entity.constants.Role;
 import com.twoclock.gitconnect.global.entity.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,4 +18,34 @@ public class Board extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String isView;
+
+    @Builder
+    public Board(Member member, String nickname, Category category, String title, String content, String isView) {
+        this.member = member;
+        this.nickname = nickname;
+        this.category = category;
+        this.title = title;
+        this.content = content;
+        this.isView = isView;
+    }
 }
