7d80e6a33313f397625ae85560c93db33d354172
openmpy <suhwan@kakao.com>
2024-07-29 12:18:01 +0900

    feat: Add Board domain entities and structure

---
+++ src/main/java/com/twoclock/gitconnect/domain/board/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.twoclock.gitconnect.domain.board.repository;
+
+import com.twoclock.gitconnect.domain.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
