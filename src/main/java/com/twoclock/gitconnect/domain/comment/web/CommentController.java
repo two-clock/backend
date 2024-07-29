cd640d9c8ebd1b0a8c5955415fd7326da2eb6fdb
openmpy <suhwan@kakao.com>
2024-07-29 12:24:27 +0900

    feat: Add comment functionality

---
+++ src/main/java/com/twoclock/gitconnect/domain/comment/web/CommentController.java
@@ -0,0 +1,11 @@
+package com.twoclock.gitconnect.domain.comment.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/comments")
+@RestController
+public class CommentController {
+}
