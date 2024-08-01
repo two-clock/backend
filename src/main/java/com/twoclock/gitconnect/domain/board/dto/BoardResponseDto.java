6e01df176f7b083c977a89edbe128c6cf01c206f
jjangsky <dbcks4646@gmail.com>
2024-08-02 00:04:32 +0900

    feat: add recode class for save Board system

---
+++ src/main/java/com/twoclock/gitconnect/domain/board/dto/BoardResponseDto.java
@@ -0,0 +1,7 @@
+package com.twoclock.gitconnect.domain.board.dto;
+
+import com.twoclock.gitconnect.domain.member.entity.Member;
+
+public class BoardResponseDto {
+
+}
--- src/main/java/com/twoclock/gitconnect/domain/board/web/BoardController.java
+++ src/main/java/com/twoclock/gitconnect/domain/board/web/BoardController.java
@@ -1,11 +1,31 @@
 package com.twoclock.gitconnect.domain.board.web;
 
+import com.twoclock.gitconnect.domain.board.dto.BoardReqeustDto;
+import com.twoclock.gitconnect.domain.board.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.twoclock.gitconnect.domain.board.dto.BoardReqeustDto.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/boards")
 @RestController
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("")
+    public ResponseEntity<?> saveBoard(@RequestBody @Valid BoardSaveReqDto boardSaveReqDto,
+                                       BindingResult bindingResult) throws BindException {
+
+
+
+        return ResponseEntity.ok().build();
+    }
 }
