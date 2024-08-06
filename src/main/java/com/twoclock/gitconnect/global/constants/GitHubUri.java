aa7890c4143148dfaa8b8dcca26bedd0d9a20f24
openmpy <suhwan@kakao.com>
2024-08-06 16:16:19 +0900

    refactor: Add HTTP methods to GitHubUri enum

--- src/main/java/com/twoclock/gitconnect/global/constants/GitHubUri.java
+++ src/main/java/com/twoclock/gitconnect/global/constants/GitHubUri.java
@@ -2,13 +2,15 @@ package com.twoclock.gitconnect.global.constants;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 
 @RequiredArgsConstructor
 @Getter
 public enum GitHubUri {
 
-    ACCESS_TOKEN("https://github.com/login/oauth/access_token?scope=user"),
-    USER_INFO("https://api.github.com/user");
+    ACCESS_TOKEN(HttpMethod.POST, "https://github.com/login/oauth/access_token?scope=user"),
+    USER_INFO(HttpMethod.GET, "https://api.github.com/user");
 
+    private final HttpMethod method;
     private final String uri;
 }
