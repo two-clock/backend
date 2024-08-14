d33a674c7b75efff8d7a2c68f19f74c7f0dc4d0a
jjangsky <dbcks4646@gmail.com>
2024-08-14 23:30:24 +0900

    fix:modify GlobalException - MethodArgumentNotValidException return Object

--- src/main/java/com/twoclock/gitconnect/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/twoclock/gitconnect/global/exception/GlobalExceptionHandler.java
@@ -5,19 +5,19 @@ import com.twoclock.gitconnect.global.slack.annotation.SlackNotification;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -93,16 +93,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDto> MethodArgumentNotValidException(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
-        List<String> errorMessages = bindingResult.getAllErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .toList();
-        String errorMessage = String.join(", ", errorMessages);
+        Map<String, String> errorMap = new HashMap<>();
+        for (FieldError error : bindingResult.getFieldErrors()) {
+            errorMap.put(error.getField(), error.getDefaultMessage());
+        }
 
         ErrorResponseDto responseDto =
                 new ErrorResponseDto(
                         ErrorCode.BAD_REQUEST.getCode(),
-                        errorMessage,
-                        null
+                        "유효성 검사 실패",
+                        errorMap
                 );
 
         log.error(e.getMessage(), e);
