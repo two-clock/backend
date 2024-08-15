51a2cf2be82df49675c2ce1b692209bbebea81bf
jjangsky <dbcks4646@gmail.com>
2024-08-15 22:02:53 +0900

    feat: add Github Follower list Api using access token

--- src/main/java/com/twoclock/gitconnect/openapi/github/service/GithubAPISerivce.java
+++ src/main/java/com/twoclock/gitconnect/openapi/github/service/GithubAPISerivce.java
@@ -1,9 +1,51 @@
 package com.twoclock.gitconnect.openapi.github.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twoclock.gitconnect.global.constants.GitHubUri;
+import com.twoclock.gitconnect.global.util.RestClientUtil;
+import com.twoclock.gitconnect.openapi.github.dto.FollowRespDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class GithubAPISerivce {
+
+    private final ObjectMapper objectMapper;
+
+    public List<FollowRespDto> getFollowers(String accessToken) throws JsonProcessingException {
+        HttpHeaders headers = getHeaders(accessToken);
+        String result = RestClientUtil.get(GitHubUri.FOLLOWER_LIST.getUri(), headers);
+        return getFollowersFromJson(result);
+    }
+
+    private HttpHeaders getHeaders(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", "application/vnd.github+json");
+        headers.add("X-GitHub-Api-Version", "2022-11-28");
+        headers.add("Authorization", "Bearer " + accessToken);
+        return headers;
+    }
+
+    private List<FollowRespDto> getFollowersFromJson(String result) throws JsonProcessingException {
+        try {
+            List<FollowRespDto> users = new ArrayList<>();
+            JsonNode arrayNode  = objectMapper.readTree(result);
+            for (JsonNode node : arrayNode) {
+                String login = node.path("login").asText();
+                String avatarUrl = node.path("avatar_url").asText();
+                String gravatarId = node.path("html_url").asText();
+                users.add(new FollowRespDto(login, avatarUrl, gravatarId));
+            }
+            return users;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/twoclock/gitconnect/openapi/github/web/GithubAPIController.java
+++ src/main/java/com/twoclock/gitconnect/openapi/github/web/GithubAPIController.java
@@ -1,9 +1,16 @@
 package com.twoclock.gitconnect.openapi.github.web;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.twoclock.gitconnect.global.model.RestResponse;
+import com.twoclock.gitconnect.openapi.github.dto.FollowRespDto;
 import com.twoclock.gitconnect.openapi.github.service.GithubAPISerivce;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/github")
@@ -11,4 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class GithubAPIController {
 
     private final GithubAPISerivce githubAPISerivce;
+
+    // 임시용
+    @Value("${github.access-token}")
+    private String accessToken;
+
+    @GetMapping("/followers")
+    public RestResponse getFollowers() throws JsonProcessingException {
+        // TODO : 토큰에서 Github Access Token을 추출 해야함.
+        List<FollowRespDto> result = githubAPISerivce.getFollowers(accessToken);
+        return new RestResponse(result);
+    }
 }
