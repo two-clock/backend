3db87bea8e70ccfb629ec78b91f836112c103729
jjangsky <dbcks4646@gmail.com>
2024-08-15 17:39:21 +0900

    feat: add Github Api to Enum

--- src/main/java/com/twoclock/gitconnect/global/constants/GitHubUri.java
+++ src/main/java/com/twoclock/gitconnect/global/constants/GitHubUri.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpMethod;
 public enum GitHubUri {
 
     ACCESS_TOKEN(HttpMethod.POST, "https://github.com/login/oauth/access_token?scope=user"),
-    USER_INFO(HttpMethod.GET, "https://api.github.com/user");
+    USER_INFO(HttpMethod.GET, "https://api.github.com/user"),
+    FOLLOWER_LIST(HttpMethod.GET, "https://api.github.com/user/followers"),
+    FOLLOWING_LIST(HttpMethod.GET, "https://api.github.com/user/following");
 
     private final HttpMethod method;
     private final String uri;
