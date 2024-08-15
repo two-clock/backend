e66e7f1ec6d507b717e7788385649a9dacdfa2a7
openmpy <suhwan@kakao.com>
2024-08-15 16:47:51 +0900

    feat: Add MultipartJackson2HttpMessageConverter class

---
+++ src/main/java/com/twoclock/gitconnect/global/converter/MultipartJackson2HttpMessageConverter.java
@@ -0,0 +1,31 @@
+package com.twoclock.gitconnect.global.converter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+
+@Component
+public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    public MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    protected boolean canWrite(MediaType mediaType) {
+        return false;
+    }
+}
