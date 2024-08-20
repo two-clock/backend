955388ab17808e2d9fb35dd1018b06350f43a6bc
jjangsky <dbcks4646@gmail.com>
2024-08-21 00:02:56 +0900

    feat: regist comment api to Board

--- src/main/java/com/twoclock/gitconnect/domain/comment/service/CommentService.java
+++ src/main/java/com/twoclock/gitconnect/domain/comment/service/CommentService.java
@@ -1,12 +1,49 @@
 package com.twoclock.gitconnect.domain.comment.service;
 
+import com.twoclock.gitconnect.domain.board.entity.Board;
+import com.twoclock.gitconnect.domain.board.repository.BoardRepository;
+import com.twoclock.gitconnect.domain.comment.dto.CommentRegReqDto;
+import com.twoclock.gitconnect.domain.comment.entity.Comment;
 import com.twoclock.gitconnect.domain.comment.repository.CommentRepository;
+import com.twoclock.gitconnect.domain.member.entity.Member;
+import com.twoclock.gitconnect.domain.member.repository.MemberRepository;
+import com.twoclock.gitconnect.global.exception.CustomException;
+import com.twoclock.gitconnect.global.exception.constants.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final MemberRepository memberRepository;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public void saveComment(CommentRegReqDto dto, String githubId) {
+        Board board = validateBoard(dto.boardId());
+        Member member = validateMember(githubId);
+
+        Comment comment = Comment.builder()
+                .board(board)
+                .member(member)
+                .nickname(member.getName())
+                .content(dto.content())
+                .build();
+        commentRepository.save(comment);
+    }
+
+    private Board validateBoard(Long boardId) {
+        return boardRepository.findById(boardId).orElseThrow(
+                () -> new CustomException(ErrorCode.NOT_FOUND_BOARD)
+        );
+    }
+
+    private Member validateMember(String githubId) {
+        return memberRepository.findByGitHubId(githubId).orElseThrow(
+                () -> new CustomException(ErrorCode.NOT_FOUND_MEMBER)
+        );
+    }
 }
--- src/main/java/com/twoclock/gitconnect/domain/comment/web/CommentController.java
+++ src/main/java/com/twoclock/gitconnect/domain/comment/web/CommentController.java
@@ -1,6 +1,14 @@
 package com.twoclock.gitconnect.domain.comment.web;
 
+import com.twoclock.gitconnect.domain.comment.dto.CommentRegReqDto;
+import com.twoclock.gitconnect.domain.comment.service.CommentService;
+import com.twoclock.gitconnect.global.model.RestResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/comments")
 @RestController
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public RestResponse saveComment(@RequestBody @Valid CommentRegReqDto dto,
+                                    @AuthenticationPrincipal UserDetails userDetails) {
+        String githubId = userDetails.getUsername();
+        commentService.saveComment(dto, githubId);
+        return RestResponse.OK();
+    }
 }
