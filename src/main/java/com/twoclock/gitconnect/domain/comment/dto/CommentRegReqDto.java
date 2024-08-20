bcb11cfb65f9c955adcf099e2761a862ee5df28c
jjangsky <dbcks4646@gmail.com>
2024-08-20 23:40:36 +0900

    feat: add Comment Regist RequestDto Class and add rule

---
+++ src/main/java/com/twoclock/gitconnect/domain/comment/dto/CommentRegReqDto.java
@@ -0,0 +1,14 @@
+package com.twoclock.gitconnect.domain.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CommentRegReqDto(
+        @NotNull(message = "게시판 키가 존재하지 않습니다.")
+        Long boardId,
+        @Size(max = 50, message = "댓글은 50자 이내로 작성해주세요.")
+        @NotBlank(message = "내용을 입력해주세요.")
+        String content
+) {
+}
