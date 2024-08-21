9cdd082b2256ecee372c1bf307771828df50f49a
openmpy <suhwan@kakao.com>
2024-08-22 02:17:34 +0900

    feat: Add pagination for chat messages endpoint

--- src/main/java/com/twoclock/gitconnect/domain/chat/web/ChatRoomController.java
+++ src/main/java/com/twoclock/gitconnect/domain/chat/web/ChatRoomController.java
@@ -1,11 +1,14 @@
 package com.twoclock.gitconnect.domain.chat.web;
 
+import com.twoclock.gitconnect.domain.chat.dto.ChatMessageRespDto;
 import com.twoclock.gitconnect.domain.chat.service.ChatRoomService;
 import com.twoclock.gitconnect.global.model.RestResponse;
 import com.twoclock.gitconnect.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/api/v1/chatrooms")
 @RequiredArgsConstructor
@@ -32,5 +35,17 @@ public class ChatRoomController {
         String githubId = userDetails.getUsername();
         chatRoomService.deleteChatRoom(githubId, chatRoomId);
         return RestResponse.OK();
+    }
+
+    @GetMapping("/{chatRoomId}/messages")
+    public RestResponse chatRoomMessages(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @PathVariable("chatRoomId") String chatRoomId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10", required = false) int size
+    ) {
+        String githubId = userDetails.getUsername();
+        List<ChatMessageRespDto> responseDto = chatRoomService.chatRoomMessages(githubId, chatRoomId, page, size);
+        return new RestResponse(responseDto);
     }
 }
