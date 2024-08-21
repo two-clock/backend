3208d29c253bfcf693d652a3010e9b092c4ff3af
openmpy <suhwan@kakao.com>
2024-08-22 01:24:23 +0900

    feat: Add validation to ChatMessageSaveReqDto

--- src/main/java/com/twoclock/gitconnect/domain/chat/dto/ChatMessageSaveReqDto.java
+++ src/main/java/com/twoclock/gitconnect/domain/chat/dto/ChatMessageSaveReqDto.java
@@ -1,6 +1,9 @@
 package com.twoclock.gitconnect.domain.chat.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ChatMessageSaveReqDto(
+        @NotBlank(message = "내용을 입력해주세요.")
         String message
 ) {
 }
