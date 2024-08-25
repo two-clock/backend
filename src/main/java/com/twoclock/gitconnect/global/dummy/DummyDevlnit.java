4855dd64ef52c762bfe1f22e4595ecbf4e24a83d
jjangsky <dbcks4646@gmail.com>
2024-08-25 19:43:57 +0900

    feat: functionality to create dummy comments for testing purposes in the DummyDevInit class.

--- src/main/java/com/twoclock/gitconnect/global/dummy/DummyDevlnit.java
+++ src/main/java/com/twoclock/gitconnect/global/dummy/DummyDevlnit.java
@@ -3,6 +3,8 @@ package com.twoclock.gitconnect.global.dummy;
 import com.twoclock.gitconnect.domain.board.entity.Board;
 import com.twoclock.gitconnect.domain.board.entity.constants.Category;
 import com.twoclock.gitconnect.domain.board.repository.BoardRepository;
+import com.twoclock.gitconnect.domain.comment.entity.Comment;
+import com.twoclock.gitconnect.domain.comment.repository.CommentRepository;
 import com.twoclock.gitconnect.domain.member.entity.Member;
 import com.twoclock.gitconnect.domain.member.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +26,9 @@ public class DummyDevlnit {
     // 프로덕트 출시 전에 개발자들이 사용할 더미 데이터를 초기화하는 클래스
     @Profile("local")
     @Bean
-    CommandLineRunner init(MemberRepository memberRepository, BoardRepository boardRepository) {
+    CommandLineRunner init(MemberRepository memberRepository,
+                           BoardRepository boardRepository,
+                           CommentRepository commentRepository) {
         return (args) -> {
             log.info("Dummy Data Init");
 
@@ -41,6 +45,11 @@ public class DummyDevlnit {
             createDummyBoards(boards, "저장소 홍보 테스트 게시글", Category.BD2, 10, members.get(0));
             createDummyBoards(boards, "사용자 신고 테스트 게시글", Category.BD3, 10, members.get(0));
             boardRepository.saveAll(boards);
+
+            // 테스트 댓글 생성
+            List<Comment> comments = new ArrayList<>();
+            createDummyComments(comments, 5, members.get(0), boards.get(0));
+            commentRepository.saveAll(comments);
         };
     }
 
@@ -55,6 +64,19 @@ public class DummyDevlnit {
                     .category(category)
                     .build();
             boards.add(board);
+        }
+    }
+
+    private void createDummyComments(List<Comment> comments, int count,
+                                     Member member, Board board) {
+        for (int i = 0; i < count; i++) {
+            Comment comment = Comment.builder()
+                    .board(board)
+                    .member(member)
+                    .nickname(member.getName())
+                    .content("테스트 댓글 입니다 " + i)
+                    .build();
+            comments.add(comment);
         }
     }
 
